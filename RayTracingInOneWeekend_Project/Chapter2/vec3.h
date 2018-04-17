4514a2fcd0373ee93758f917979f56187dc7ae47
328419914@qq.com <328419914@qq.com>
2018-04-17 13:11:11 +0800

    1

--- RayTracingInOneWeekend_Project/Chapter2/vec3.h
+++ RayTracingInOneWeekend_Project/Chapter2/vec3.h
@@ -17,8 +17,73 @@ public:
 	inline float b() const { return e[2]; }
 
 	inline const vec3& operator+() const { return *this; }
+	inline vec3 operator-() const { return vec3{ -e[0],-e[1], -e[2] }; }
+	inline float operator[](int i) const { return e[i]; }
+	inline float& operator[](int i) { return e[i]; }
 
+	inline vec3& operator+=(const vec3 &v2);
+	inline vec3& operator-=(const vec3 &v2);
+	inline vec3& operator*=(const vec3 &v2);
+	inline vec3& operator/=(const vec3 &v2);
+	inline vec3& operator*=(const float t);
+	inline vec3& operator/=(const float t);
+
+	inline float length() const { return sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]); }
+	inline float squared_length() const { return e[0] * e[0] + e[1] * e[1] + e[2] * e[2]; }
+	inline void make_unit_vector();
 	float e[3];
 
 	
+	
 };
+
+inline std::istream& operator>>(std::istream &is, vec3 &t) { is >> t.e[0] >> t.e[1] >> t.e[2]; return is; }
+inline std::ostream& operator<<(std::ostream &os, const vec3 &t) { os << t.e[0] << " " << t.e[1] << " " << t.e[2]; return os; }
+
+inline void vec3::make_unit_vector()
+{
+	float k = 1.0 / sqrt(e[0] * e[0] + e[1] * e[1] + e[2] * e[2]);
+	e[0] *= k; e[1] *= k; e[2] *= k;
+}
+
+inline vec3 operator+(const vec3 &v1,const vec3 &v2)
+{
+	return vec3(v1.e[0] + v2.e[0], v1.e[1] + v2.e[1], v1.e[2] + v2.e[2]);
+}
+inline vec3 operator-(const vec3 &v1, const vec3 &v2)
+{
+	return vec3(v1.e[0] - v2.e[0], v1.e[1] - v2.e[1], v1.e[2] - v2.e[2]);
+}
+inline vec3 operator*(const vec3 &v1, const vec3 &v2)
+{
+	return vec3(v1.e[0] * v2.e[0], v1.e[1] * v2.e[1], v1.e[2] * v2.e[2]);
+}
+inline vec3 operator/(const vec3 &v1, const vec3 &v2)
+{
+	return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
+}
+
+inline vec3 & vec3::operator+=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+inline vec3 & vec3::operator-=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+inline vec3 & vec3::operator*=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+inline vec3 & vec3::operator/=(const vec3 & v2)
+{
+	// TODO: 在此处插入 return 语句
+}
+inline vec3 & vec3::operator*=(const float t)
+{
+	// TODO: 在此处插入 return 语句
+}
+inline vec3 & vec3::operator/=(const float t)
+{
+	// TODO: 在此处插入 return 语句
+}
