189d0c50b073a255d49529df8d5525d5262fc5e9
Move <328419914@qq.com>
2018-04-25 10:09:00 +0800

    11

--- RayTracingInOneWeekend_Project/Chapter6/vec3.h
+++ RayTracingInOneWeekend_Project/Chapter6/vec3.h
@@ -17,7 +17,7 @@ public:
 	inline float b() const { return e[2]; }
 
 	inline const vec3& operator+() const { return *this; }
-	inline vec3 operator-() const { return vec3{ -e[0],-e[1], -e[2] }; }
+	inline vec3 operator-() const { return vec3( -e[0],-e[1], -e[2] ); }
 	inline float operator[](int i) const { return e[i]; }
 	inline float& operator[](int i) { return e[i]; }
 
